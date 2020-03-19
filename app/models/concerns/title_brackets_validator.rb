0fc1be86361f6bd55e705c47a093e180180d00ac
Dzonyy <paszcza.jakub@gmail.com>
2020-03-19 09:06:06 +0100

    finished task 4, uncoment validator tests

---
+++ app/models/concerns/title_brackets_validator.rb
@@ -0,0 +1,21 @@
+class TitleBracketsValidator < ActiveModel::Validator
+  def validate(record)
+    brackets = { '{' => '}', '(' => ')', '[' => ']' }
+    brackets_stack = []
+    valid = true
+
+    record.title.each_char.with_index do |x, i|
+      brackets_stack << [brackets[x], i] if brackets.key?(x)
+
+      if brackets.value?(x)
+        last_bracket = brackets_stack.pop
+        if last_bracket.nil? || last_bracket[0] != x || last_bracket[1] + 1 == i
+          valid = false
+          break
+        end
+      end
+    end
+
+    record.errors.add(:title, 'has unclosed bracket') unless brackets_stack.empty? && valid
+  end
+end
