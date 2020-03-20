c0cb00aac9cce9530360a826a55f309b1a4268be
Dzonyy <paszcza.jakub@gmail.com>
2020-03-20 08:50:34 +0100

    fixed comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,4 +5,5 @@ class Comment < ApplicationRecord
   validates :text,  presence: true
   validates :user_id,  presence: true
   validates :movie_id,  presence: true
+  validates :user_id, uniqueness: { scope: :movie_id }
 end
